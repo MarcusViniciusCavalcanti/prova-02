4cca911aa4062e84316332ffd6a03a18be455747
Vinicius Cavalcanti <vincius.cavalcanti16@gmail.com>
2018-12-04 21:45:46 -0200

    test heroku

--- src/main/java/br/edu/utfpr/tsi/prova02/Prova02Application.java
+++ src/main/java/br/edu/utfpr/tsi/prova02/Prova02Application.java
@@ -1,12 +1,18 @@
 package br.edu.utfpr.tsi.prova02;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Prova02Application {
+public class Prova02Application implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(Prova02Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
     }
 }
